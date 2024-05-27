dbd10d0a4e57af170c086b0cb0bba22178c3f058
ProfessorNova <114916947+ProfessorNova@users.noreply.github.com>
2024-05-27 16:19:21 +0200

    feat: Add DCF77 state machine and error handling

--- lab3-Funkuhr-Vorlage/Sources/dcf77.c
+++ lab3-Funkuhr-Vorlage/Sources/dcf77.c
@@ -29,6 +29,12 @@ DCF77EVENT dcf77Event = NODCF77EVENT;
 // Modul internal global variables
 static int  dcf77Year=2017, dcf77Month=1, dcf77Day=1, dcf77Hour=0, dcf77Minute=0;       //dcf77 Date and time as integer values
 
+// Variables for the DCF77 state machine
+static int currentBit = 0;
+static char dcf77Buffer[59];
+static char ERROR = 0;
+static char paritySum = 0;
+
 // Prototypes of functions simulation DCF77 signals, when testing without
 // a DCF77 radio signal receiver
 void initializePortSim(void);                   // Use instead of initializePort() for testing
@@ -125,9 +131,9 @@ DCF77EVENT sampleSignalDCF77(int currentTime)
         event = NODCF77EVENT;
     }
 
-    // Toggle LED B.3 based on signal state
+    // Toggle LED B.1 when the signal is low
     if (event != NODCF77EVENT) {
-        PORTB ^= 0x08; // Toggle PB3 (LED B.3)
+        PORTB ^= 0x02; // Toggle LED B.1
     }
 
     return event;
@@ -141,7 +147,117 @@ DCF77EVENT sampleSignalDCF77(int currentTime)
 void processEventsDCF77(DCF77EVENT event)
 {
 // --- Add your code here ----------------------------------------------------
-// --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? ---
+    switch (event)
+    {
+    case VALIDSECOND:
+        currentBit++;
+        break;
+    case VALIDZERO:
+        dcf77Buffer[currentBit] = 0;
+        break;
+    case VALIDONE:
+        dcf77Buffer[currentBit] = 1;
+        break;
+    case VALIDMINUTE:
+        if (currentBit == 58) {
+            currentBit = 0;
+            // check parity
+            // 21 - 27
+            paritySum = 0;
+            for (int i = 21; i <= 28; i++) {
+                paritySum += dcf77Buffer[i];
+            }
+            if (paritySum % 2 != 0) {
+                ERROR = 1;
+                break;
+            }
+            // 29 - 34
+            paritySum = 0;
+            for (int i = 29; i <= 35; i++) {
+                paritySum += dcf77Buffer[i];
+            }
+            if (paritySum % 2 != 0) {
+                ERROR = 1;
+                break;
+            }
+            // 36 - 57
+            paritySum = 0;
+            for (int i = 36; i <= 58; i++) {
+                paritySum += dcf77Buffer[i];
+            }
+            if (paritySum % 2 != 0) {
+                ERROR = 1;
+                break;
+            }
+            // decode minutes
+            dcf77Minute = 0;
+            dcf77Minute += dcf77Buffer[21] * 1;
+            dcf77Minute += dcf77Buffer[22] * 2;
+            dcf77Minute += dcf77Buffer[23] * 4;
+            dcf77Minute += dcf77Buffer[24] * 8;
+            dcf77Minute += dcf77Buffer[25] * 10;
+            dcf77Minute += dcf77Buffer[26] * 20;
+            dcf77Minute += dcf77Buffer[27] * 40;
 
+            // decode hours
+            dcf77Hour = 0;
+            dcf77Hour += dcf77Buffer[29] * 1;
+            dcf77Hour += dcf77Buffer[30] * 2;
+            dcf77Hour += dcf77Buffer[31] * 4;
+            dcf77Hour += dcf77Buffer[32] * 8;
+            dcf77Hour += dcf77Buffer[33] * 10;
+            dcf77Hour += dcf77Buffer[34] * 20;
+
+            // decode day
+            dcf77Day = 0;
+            dcf77Day += dcf77Buffer[36] * 1;
+            dcf77Day += dcf77Buffer[37] * 2;
+            dcf77Day += dcf77Buffer[38] * 4;
+            dcf77Day += dcf77Buffer[39] * 8;
+            dcf77Day += dcf77Buffer[40] * 10;
+            dcf77Day += dcf77Buffer[41] * 20;
+
+            // decode month
+            dcf77Month = 0;
+            dcf77Month += dcf77Buffer[45] * 1;
+            dcf77Month += dcf77Buffer[46] * 2;
+            dcf77Month += dcf77Buffer[47] * 4;
+            dcf77Month += dcf77Buffer[48] * 8;
+            dcf77Month += dcf77Buffer[49] * 10;
+
+            // decode year
+            dcf77Year = 0;
+            dcf77Year += dcf77Buffer[50] * 1;
+            dcf77Year += dcf77Buffer[51] * 2;
+            dcf77Year += dcf77Buffer[52] * 4;
+            dcf77Year += dcf77Buffer[53] * 8;
+            dcf77Year += dcf77Buffer[54] * 10;
+            dcf77Year += dcf77Buffer[55] * 20;
+            dcf77Year += dcf77Buffer[56] * 40;
+            dcf77Year += dcf77Buffer[57] * 80;
+            dcf77Year += 2000;
+        } else {
+            currentBit = 0;
+            ERROR = 1;
+            break;
+        }
+        ERROR = 0;
+        setClock((char) dcf77Hour, (char) dcf77Minute, 0);
+        break;
+    default:
+        break;
+    }
+
+    if (ERROR) {
+        // TURN ON LED B.2
+        PORTB |= 0x04;
+        // TURN OFF LED B.3
+        PORTB &= ~0x08;
+    } else {
+        // TURN OFF LED B.2
+        PORTB &= ~0x04;
+        // TURN ON LED B.3
+        PORTB |= 0x08;
+    }
 }
 
